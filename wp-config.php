8c8801e2590d218d907b2f93c704e79b35a2c041
Elizabet Hunt <ezoehunt@gmail.com>
2012-08-16 03:10:43 -0700

    access

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'neighborhow-pagoda');
+define('DB_NAME', 'neighborhowpagoda');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
