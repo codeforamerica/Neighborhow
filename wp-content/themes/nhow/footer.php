f12e9655ef2b637ca815ff9f2c02fb095033052b
Elizabet Hunt <ezoehunt@gmail.com>
2012-08-21 01:56:55 -0700

    back to basics

--- wp-content/themes/nhow/footer.php
+++ wp-content/themes/nhow/footer.php
@@ -1,43 +1,40 @@
 <?php
+global $style_url;
+global $app_url;
 $style_url = get_bloginfo('stylesheet_directory');
 $app_url = get_bloginfo('url');
 ?>
-			<div class="span4 odd clearfix">
-				<h3 class="footer-header" style="text-align:center;">About Neighborhow</h3>
-				<ul>
-					<li><a class="noline footer-link" title="See how Neighborhow works" href="<?php echo $app_url;?>/about">How It Works</a></li>
-					<li><a class="noline footer-link" title="Read Frequently Asked Questions" href="<?php bloginfo('url');?>/faqs">FAQs</a></li>						
-				</ul>
-			</div><!-- /span4 -->
-		
-			<div class="span4 even clearfix">
-				<h3 class="footer-header" style="text-align:center;">Contact</h3>
-				<ul class="unstyled flickr">
-				<li><a class="noline footer-link" title="Contact" href="<?php bloginfo('url');?>/contact">Email Us</a></li>
-					<li><a class="noline footer-link" title="Read the blog" href="<?php bloginfo('url');?>/blog">Read the Blog</a></li>					
-				<li>
-					<ul class="footer-social">
-						<li><a target="_blank" title="Follow Neighborhow on Twitter" href="http://www.twitter.com/!/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/twitter.png" alt="Twitter logo" width="26" /></a></li>
-						<li><a target="_blank" title="Like Neighborhow on Facebook" href="http://www.facebook.com/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/fb.png" alt="Facebook logo" width="26" /></a></li>
-						<li><a target="_blank" title="Visit Neighborhow on Github" href="http://www.github.com/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/github.png" alt="Github logo" width="26" /></a></li>
-					</ul>
-				</li>
-				</ul>			
-			</div><!-- /span4 -->			
-		
-			<div class="span4 odd clearfix">
-				<h3 class="footer-header" style="text-align:center;">Partners</h3>
-				<!--p>Neighborhow is brought to you by Code for America and the City of Philadelphia.<br/--><a target="_blank" href="http://www.phila.gov" title="Go to City of Philadelphia"><img style="float:left;" width="70" src="<?php echo $style_url;?>/images/logo_phl.png" alt="City of Philadelphia logo"></a> <a target="_blank" href="http://www.codeforamerica.org" title="Go to Code for America"><img style="float:left;position:relative;top:1.9em;margin-left:1em;" width="70" src="<?php echo $style_url;?>/images/logo_cfa.png" alt="Code for America logo"></a> </p>
-			</div><!-- /span4 -->
-		
-			<!--div class="span12 footer-copy">&copy;Neighborhow 2012</div-->
-
-
+<div class="row-fluid row-footer">
+	<div id="footer2">
+		<div class="span4 odd clearfix">
+			<h3 class="footer-header" style="text-align:center;">About Neighborhow</h3>
+			<ul>
+				<li><a class="noline footer-link" title="See how Neighborhow works" href="<?php echo $app_url;?>/about">How It Works</a></li>
+				<li><a class="noline footer-link" title="Read Frequently Asked Questions" href="<?php bloginfo('url');?>/faqs">FAQs</a></li>						
+			</ul>
+		</div><!-- /span4 -->
 	
-	</div></wrap>
-</div></container>
-
-<?php wp_footer(); ?>
+		<div class="span4 even clearfix">
+			<h3 class="footer-header" style="text-align:center;">Contact</h3>
+			<ul class="unstyled flickr">
+			<li><a class="noline footer-link" title="Contact" href="<?php bloginfo('url');?>/contact">Email Us</a></li>
+				<li><a class="noline footer-link" title="Read the blog" href="<?php bloginfo('url');?>/blog">Read the Blog</a></li>					
+			<li>
+				<ul class="footer-social">
+					<li><a target="_blank" title="Follow Neighborhow on Twitter" href="http://www.twitter.com/!/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/twitter.png" alt="Twitter logo" width="26" /></a></li>
+					<li><a target="_blank" title="Like Neighborhow on Facebook" href="http://www.facebook.com/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/fb.png" alt="Facebook logo" width="26" /></a></li>
+					<li><a target="_blank" title="Visit Neighborhow on Github" href="http://www.github.com/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/github.png" alt="Github logo" width="26" /></a></li>
+				</ul>
+			</li>
+			</ul>			
+		</div><!-- /span4 -->			
+	
+		<div class="span4 odd clearfix">
+			<h3 class="footer-header" style="text-align:center;">Partners</h3>
+			<!--p>Neighborhow is brought to you by Code for America and the City of Philadelphia.<br/--><a target="_blank" href="http://www.phila.gov" title="Go to City of Philadelphia"><img style="float:left;" width="70" src="<?php echo $style_url;?>/images/logo_phl.png" alt="City of Philadelphia logo"></a> <a target="_blank" href="http://www.codeforamerica.org" title="Go to Code for America"><img style="float:left;position:relative;top:1.9em;margin-left:1em;" width="70" src="<?php echo $style_url;?>/images/logo_cfa.png" alt="Code for America logo"></a> </p>
+		</div><!-- /span4 -->
+	</div><!--/ footer-->
+</div><!--/ row-footer-->
 
 <!-- Le javascript
 ================================================== -->
@@ -66,7 +63,6 @@ $app_url = get_bloginfo('url');
 
 <!--script type="text/javascript" src="<?php //bloginfo('stylesheet_directory');?>/lib/js/fancybox/source/jquery.fancybox.pack.js"></script-->
 
-
 <script>
 $(document).ready(function() {
 /*	$('.fancybox').fancybox({
@@ -76,13 +72,13 @@ $(document).ready(function() {
 		helpers : {
 			title : null            
 			}
-		});
+	});
 */
-		$('.dropdown-toggle').dropdown();
+	$('.dropdown-toggle').dropdown();
 		
-		
+// STOP HERE		
 });
-</script>
+</script>	
 
 </body>
 </html>
--- wp-content/themes/nhow/index.php
+++ wp-content/themes/nhow/index.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-<?php if (is_user_logged_in()) : 
+<?php if (is_user_logged_in()) { 
 //TODO - 
 // change to if user is cookied
 ?>
@@ -12,9 +12,15 @@
 		</p>			
 	</div><!--/ site-promo-->
 </div><!--/ row-promo-->
-<?php endif; ?>
-
-<div class="row-fluid row-content">
+<div class="row-fluid row-content with-orange">
+<?php
+}
+else {
+?>
+<div class="row-fluid row-content with-green">
+<?php
+}
+?>	
 	<div id="main">
 		<div id="content">
 			<div class="hfeed">
@@ -115,82 +121,4 @@ endwhile;
 <?php get_sidebar('primary'); ?>
 	</div><!--/ main-->
 </div><!--/ row-content-->
-		
-<div class="row-fluid row-footer">
-	<div id="footer2">
-		<div class="span4 odd clearfix">
-			<h3 class="footer-header" style="text-align:center;">About Neighborhow</h3>
-			<ul>
-				<li><a class="noline footer-link" title="See how Neighborhow works" href="<?php echo $app_url;?>/about">How It Works</a></li>
-				<li><a class="noline footer-link" title="Read Frequently Asked Questions" href="<?php bloginfo('url');?>/faqs">FAQs</a></li>						
-			</ul>
-		</div><!-- /span4 -->
-	
-		<div class="span4 even clearfix">
-			<h3 class="footer-header" style="text-align:center;">Contact</h3>
-			<ul class="unstyled flickr">
-			<li><a class="noline footer-link" title="Contact" href="<?php bloginfo('url');?>/contact">Email Us</a></li>
-				<li><a class="noline footer-link" title="Read the blog" href="<?php bloginfo('url');?>/blog">Read the Blog</a></li>					
-			<li>
-				<ul class="footer-social">
-					<li><a target="_blank" title="Follow Neighborhow on Twitter" href="http://www.twitter.com/!/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/twitter.png" alt="Twitter logo" width="26" /></a></li>
-					<li><a target="_blank" title="Like Neighborhow on Facebook" href="http://www.facebook.com/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/fb.png" alt="Facebook logo" width="26" /></a></li>
-					<li><a target="_blank" title="Visit Neighborhow on Github" href="http://www.github.com/neighborhow"><img src="<?php echo $style_url;?>/images/icons/social/github.png" alt="Github logo" width="26" /></a></li>
-				</ul>
-			</li>
-			</ul>			
-		</div><!-- /span4 -->			
-	
-		<div class="span4 odd clearfix">
-			<h3 class="footer-header" style="text-align:center;">Partners</h3>
-			<!--p>Neighborhow is brought to you by Code for America and the City of Philadelphia.<br/--><a target="_blank" href="http://www.phila.gov" title="Go to City of Philadelphia"><img style="float:left;" width="70" src="<?php echo $style_url;?>/images/logo_phl.png" alt="City of Philadelphia logo"></a> <a target="_blank" href="http://www.codeforamerica.org" title="Go to Code for America"><img style="float:left;position:relative;top:1.9em;margin-left:1em;" width="70" src="<?php echo $style_url;?>/images/logo_cfa.png" alt="Code for America logo"></a> </p>
-		</div><!-- /span4 -->
-	</div><!--/ footer-->
-</div><!--/ row-footer-->
-
-<!-- Le javascript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/jquery.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-collapse.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-transition.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-alert.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-modal.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-dropdown.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-scrollspy.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-tab.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-tooltip.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-popover.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-button.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-carousel.js"></script>
-<script src="<?php bloginfo('stylesheet_directory'); ?>/lib/js/bootstrap-typeahead.js"></script>
-<script src="<?php bloginfo('stylesheet_directory');?>/lib/js/application.js"></script>
-
-<!--script type="text/javascript" src="<?php //bloginfo('stylesheet_directory');?>/lib/js/jquery.easing.1.2.js"></script>
-<script type='text/javascript' src='<?php //bloginfo('stylesheet_directory');?>/lib/js/jquery.scrollTo-1.4.2-min.js'></script>
-<script type='text/javascript' src='<?php //bloginfo('stylesheet_directory');?>/lib/js/jquery.localscroll-1.2.7-min.js'></script-->
-
-<!-- History.js --> 
-<!--script defer src="<?php //bloginfo('stylesheet_directory');?>/lib/js/hashchange.js" type="text/javascript"></script-->
-
-<!--script type="text/javascript" src="<?php //bloginfo('stylesheet_directory');?>/lib/js/fancybox/source/jquery.fancybox.pack.js"></script-->
-
-<script>
-$(document).ready(function() {
-/*	$('.fancybox').fancybox({
-		autosize:false,
-		height:340,
-		width:500,
-		helpers : {
-			title : null            
-			}
-	});
-*/
-	$('.dropdown-toggle').dropdown();
-		
-// STOP HERE		
-});
-</script>	
-
-</body>
-</html>
+<?php get_footer(); ?>		
