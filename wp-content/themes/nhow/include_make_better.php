a1f8a562d474f4752864ff74629d33073ab40240
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-13 01:53:14 -0700

    final response

--- wp-content/themes/nhow/include_make_better.php
+++ wp-content/themes/nhow/include_make_better.php
@@ -5,12 +5,12 @@ $app_url = get_bloginfo('url');
 <div class="widget-side">
 	<h5 class="widget-title">Make Neighborhow Better</h5>
 	<div class="widget-copy">
-		<ul>
-			<li><a class="nhline" href="<?php echo $app_url;?>/contact" title="Submit a story">Submit a story</a> about something you&#39;ve done to improve your neighborhood.</li>							
-			<li><a class="nhline" href="<?php echo $app_url;?>/feedback" title="Give feedback">Give us your feedback.</a></li>
-			<li>Decide what&#39;s next for Neighborhow &#8212; <a class="nhline" href="<?php echo $app_url;?>/blog" title="Help decide what&#39;s next for Neighborhow">tell us the content and features you want.</a></li>
-			<li>If you&#39;re a city who wants Neighborhow in your city, <a class="nhline" href="<?php echo $app_url;?>/contact" title="Contact us">contact us</a>!</li>
-			<!--li>Other questions or comments? <a class="nhline" href="<?php echo $app_url;?>/contact" title="Contact us">Contact us</a>.</li-->
+		<ul class="bullets">
+			<li class="bullets"><a class="nhline" href="<?php echo $app_url;?>/contact" title="Submit a story">Submit a story</a> about something you&#39;ve done to improve your neighborhood.</li>							
+			<li class="bullets"><a class="nhline" href="<?php echo $app_url;?>/feedback" title="Give feedback">Give us your feedback.</a></li>
+			<li class="bullets">Decide what&#39;s next for Neighborhow &#8212; <a class="nhline" href="<?php echo $app_url;?>/blog" title="Help decide what&#39;s next for Neighborhow">tell us the content and features you want.</a></li>
+			<li class="bullets">If you&#39;re a city who wants Neighborhow in your city, <a class="nhline" href="<?php echo $app_url;?>/contact" title="Contact us">contact us</a>!</li>
+			<!--li>Other questions or comments? <a class="nhline" href="<?php //echo $app_url;?>/contact" title="Contact us">Contact us</a>.</li-->
 		</ul>
 	</div>
 </div><!--/ widget-->
