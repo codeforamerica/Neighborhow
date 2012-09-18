bef3f9a4349443b1253c615476ffa8fd20689136
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-17 22:08:09 -0700

    testing more

--- wp-content/themes/nhow/sidebar-guides.php
+++ wp-content/themes/nhow/sidebar-guides.php
@@ -88,6 +88,7 @@ if (have_comments()) {
 <?php 
 if (lip_user_has_loved_post($current_user->ID, $post->ID)) {
 	echo '<a id="likedthis" title="See your other Likes" href="'.$app_url.'/author/'.$current_user->user_login.'" class="likedthis nhline">You like this</a>';
+	echo '</p>';
 }
 else {
 	lip_love_it_link();
@@ -95,14 +96,6 @@ else {
 }
 ?>
 <?php if ( function_exists( 'sharing_display' ) ) echo sharing_display(); ?>
-				
-				<!--p class="side-buttons">
-					
-
-
-					<span class='st_facebook_large' displayText='Facebook' style="margin -top:-2em;"></span>
-				<span class='st_twitter_large' displayText='Tweet'></span>
-				<span class='st_email_large' displayText='Email'></span></p-->
 				
 				<ul class="gde-actions">	
 					<li><a class="nhline" href="#leavecomment" title="Add Your Comment">Add a Comment</a></li>											
