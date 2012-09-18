d0940a8a936d8f0db83b124b52a96da584b74186
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-18 13:56:03 -0700

    side

--- wp-content/themes/nhow/sidebar-guides.php
+++ wp-content/themes/nhow/sidebar-guides.php
@@ -79,7 +79,7 @@ if (have_comments()) {
 		<!--h5 class="widget-title">Tools</h5-->			
 		<div class="widget-copy">
 			<div class="guide-details">		
-				<p class="side-buttons">
+				<p class="side-buttons" style="border:1px solid red !important;">
 <?php 
 if (lip_user_has_loved_post($current_user->ID, $post->ID)) {
 	echo '<a id="likedthis" title="See your other Likes" href="'.$app_url.'/author/'.$current_user->user_login.'" class="likedthis nhline">You like this</a>';
@@ -89,10 +89,7 @@ else {
 }
 ?>
 <?php if ( function_exists( 'sharing_display' ) ) echo sharing_display(); ?>
-<?php
-	echo '</p>';
-?>
-				
+				</p>
 				<ul class="gde-actions">	
 					<li><a class="nhline" href="#leavecomment" title="Add Your Comment">Add a Comment</a></li>											
 					<!--li><a class="nhline" href="" title="">Add a Tip</a></li-->
