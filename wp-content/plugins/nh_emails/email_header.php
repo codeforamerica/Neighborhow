56976a9cf7e10f15328b22b7a0bb514e43486f73
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-19 03:07:05 -0700

    email

--- wp-content/plugins/nh_emails/email_header.php
+++ wp-content/plugins/nh_emails/email_header.php
@@ -13,15 +13,15 @@
 	<tr>
 		<td style="padding:15px;">
 		<center>
-		<table width="550" cellpadding="0" bgcolor="#fff" cellspacing="0" align="center" border="1">
+		<table width="550" cellpadding="0" bgcolor="#fff" cellspacing="0" align="center" border="0">
 			<tr>
 				<td align="left">
 					<div style="border:solid 1px #4D946A;">
 						<table id="header" width="100%" border="0" cellpadding="0" bgcolor="#fff" cellspacing="0" style="line-height:1.6;font-size:12px;font-family: Helvetica, Arial, sans-serif;color:#555;">
 							<tr>
-								<td style="padding-left:30px;"><a href="http://www.neighborhow.org" style="text-decoration:none;" target="_blank"><img src="http://www.neighborhow.org/wp-content/themes/nhow/images/email_logo.png" height="70"></a>
+								<td style="padding-top:12px;padding-left:30px;"><a href="http://www.neighborhow.org" style="text-decoration:none;" target="_blank"><img src="http://neighborhow-pagodas.pagodabox.com/wp-content/themes/nhow/images/email_logo.png" height="70"></a>
 </td>
-								<td style="padding-top:12px;line-height:180%;padding-left:12px;padding-right:30px;" align="left" valign="baseline"><span style="font-size:18px;color:#4D946A;">Neighborhow makes it easy to find and share ways to improve your neighborhood.</span></td>												
+								<td style="padding-top:12px;padding-left:12px;padding-right:30px;" align="left" valign="baseline"><span style="font-size:18px;color:#4D946A;">Neighborhow makes it easy to find and share ways to improve your neighborhood.</span></td>												
 							</tr>
 						</table>
 						<table id="content" width="490" border="0" cellpadding="0" bgcolor="#fff" cellspacing="0" style="margin-top:15px;margin-right:30px; margin-left:30px;color:#555;line-height:1.6;font-size:12px;font-family: Arial, sans-serif;">
@@ -29,4 +29,4 @@
 								<td colspan="2" style="border-top: solid 1px #4996a4">
 									<!--content-->
 									<div style="padding:15px 0;">
-										<h4 style="padding:5px 0 0 0; font-family:Helvetica, Arial, sans-serif;font-weight:500;font-size:18px;color:#4D946A;border-bottom:1px solid #ddd"><?php echo $email_subject ?></h4>
+										<h4 style="font-family:Helvetica, Arial, sans-serif;font-weight:500;font-size:18px;color:#4D946A;border-bottom:1px solid #ddd"><?php echo $email_subject ?></h4>
