ccc72d943e22fd0c169be722a1a3e5f5ff38620b
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-21 01:30:00 -0700

    more icons

--- wp-content/themes/nhow/category-guides.php
+++ wp-content/themes/nhow/category-guides.php
@@ -26,20 +26,22 @@ if ($promo_query->have_posts()) :
 while($promo_query->have_posts()) : $promo_query->the_post();
 $imgSrc = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
 ?>
-<li class="thumb-archive" id="post-<?php echo $post->ID; ?>">
-	<div class="thumbnail thumbnail-wrapper" style="float:left;margin-right:1em;"><a rel="bookmark" title="View <?php echo the_title();?>" href="<?php the_permalink();?>"><img src="<?php bloginfo('stylesheet_directory');?>/lib/timthumb.php?src=<?php echo $imgSrc[0];?>&w=180&h=140&zc=1&at=t" alt="Photo from <?php echo the_title();?>" /></a>
-		<div class="caption">
+<li id="post-<?php echo $post->ID;?>"><a class="nhline" rel="bookmark" title="See <?php echo the_title();?>" href="<?php the_permalink();?>"><img src="<?php echo $style_url;?>/lib/timthumb.php?src=<?php echo $imgSrc[0];?>&w=180&zc=1&at=t" alt="Photo from <?php echo the_title();?>" />
+	
+	
+<!--li class="thumb-archive" id="post-<?php echo $post->ID; ?>">
+	<div class="thumbnail thumbnail-wrapper" style="float:left;margin-right:1em;"><a rel="bookmark" title="View <?php echo the_title();?>" href="<?php the_permalink();?>"><img src="<?php bloginfo('stylesheet_directory');?>/lib/timthumb.php?src=<?php echo $imgSrc[0];?>&w=180&h=140&zc=1&at=t" alt="Photo from <?php echo the_title();?>" /></a-->
+	<div class="home-caption">
 <?php
 $pad = ' ...';
 $pic_title = trim_by_chars(get_the_title(),'60',$pad);
 ?>
-		<h5><a class="noline" rel="bookmark" href="<?php the_permalink();?>" title="View <?php the_title();?>"><?php echo $pic_title;?></a></h5>
-		</div>
-	</div>
+		<p><a class="nhline" href="<?php echo get_permalink();?>" title="See <?php echo the_title();?>"><strong><?php echo $pic_title;?></strong></a></p>
+	</div>	
 </li>
 <?php endwhile; ?>	
 <?php else : ?>	
-nothing to see here no guides
+<li id="post-no-guides">Sorry, there are no public Neighborhow Guides to see at this time.</li>
 <?php endif; ?>	
 </ul>							
 		
