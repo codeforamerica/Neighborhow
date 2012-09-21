aed1ada36001809275541b17b8af8aa7dfb4d8a4
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-21 04:11:56 -0700

    jetpack

--- wp-content/themes/nhow/sidebar-idea-single.php
+++ wp-content/themes/nhow/sidebar-idea-single.php
@@ -68,7 +68,10 @@ else {
 <?php 
 // Turn on if function when workign locally - doesnt work hosted
 //if ( function_exists( 'nh_sharing_display' ) ) 
-echo sharing_display(); ?>
+echo '<div class="jetpack-idea-single">';
+echo sharing_display(); 
+echo '</div>';
+?>
 				<p class="sharing-jp"><a class="nhline" href="#leavecomment" title="Add Your Comment">Add a Comment</a></p>
 
 			</div><!--guide details-->
