ee0b14470d6cd6b69ed3c1432fb74f1fcf3b4eae
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-22 11:41:18 -0700

    paths

--- wp-content/themes/nhow/lib/paths.php
+++ wp-content/themes/nhow/lib/paths.php
@@ -2,28 +2,24 @@
 
 function get_bodyid() {
 
-// 	PREP
-$term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); 
-$nh_term = $term->name;
-
-$categories = get_the_category();
-foreach ($categories as $category) {
-	$cat_id = $category->term_id;
-	$cat_parent_id = $category->category_parent;
-}
-if ($cat_parent_id) {
-	$cat_name = strtolower(get_the_category_by_id($cat_parent_id));
-}
-else {
-	$cat_name = strtolower(get_the_category_by_id($cat_id));
-}
-
 
 	if (is_home()) { 
 		$bodyid = 'home'; 
 	}
 	
 	elseif (is_archive() AND !is_author() AND !is_tax()) {
+		$categories = get_the_category();
+		foreach ($categories as $category) {
+			$cat_id = $category->term_id;
+			$cat_parent_id = $category->category_parent;
+		}
+		if ($cat_parent_id) {
+			$cat_name = strtolower(get_the_category_by_id($cat_parent_id));
+		}
+		else {
+			$cat_name = strtolower(get_the_category_by_id($cat_id));
+		}
+		
 		$bodyid = $cat_name;
 	}
 	
@@ -32,6 +28,9 @@ else {
 	}
 	
 	elseif (is_archive() AND is_tax('nh_cities')) {
+		$term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); 
+		$nh_term = $term->name;
+		
 		$bodyid = 'cities-'.$nh_term;
 	}
 	
@@ -52,9 +51,22 @@ else {
 	}
 	
 	elseif (is_single()) {
-		if (isset($cat_name)) {
+		$categories = get_the_category();
+		foreach ($categories as $category) {
+			$cat_id = $category->term_id;
+			$cat_parent_id = $category->category_parent;
+		}
+		if ($cat_parent_id) {
+			$cat_name = strtolower(get_the_category_by_id($cat_parent_id));
 			$bodyid = $cat_name;
-		}	
+		}
+		else {
+			$cat_name = strtolower(get_the_category_by_id($cat_id));
+			$bodyid = $cat_name;
+		}
+//		if (isset($cat_name)) {
+//			$bodyid = $cat_name;
+//		}	
 	}
 	
 	elseif (is_search()) {
