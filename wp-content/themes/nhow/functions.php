9056446cd35b9783300cbd49355f6f342e8eb4b0
Elizabet Hunt <ezoehunt@gmail.com>
2012-09-23 10:19:06 -0700

    idea longer title

--- wp-content/themes/nhow/functions.php
+++ wp-content/themes/nhow/functions.php
@@ -133,8 +133,8 @@ function nh_validate_frm($errors, $posted_field, $posted_value) {
 
 // Feedback Title
 		if ($posted_field->id == 706 AND !empty($posted_value)) { 
-			if (strlen($posted_value) > 50 AND !empty($posted_value)) {
-				$errors['field'. $posted_field->id] = '<strong>ERROR</strong>: Please enter a title that is fewer than 50 characters.';
+			if (strlen($posted_value) > 75 AND !empty($posted_value)) {
+				$errors['field'. $posted_field->id] = '<strong>ERROR</strong>: Please enter a title that is fewer than 75 characters.';
 			}
 			if (!preg_match("/^[a-zA-Z0-9 \"?\\\',-]+$/", $posted_value) AND !empty($posted_value)) {
 				$errors['field'. $posted_field->id] = '<strong>ERROR</strong>: Invalid characters. Please enter a title using only letters, numbers, space, hyphen, comma, and apostrophe.';	
